5ab036365dfdb134c2c3c6396e536b2f6eb53b88
Francesco Gallo <francescogallogit@gmail.com>
2026-06-24 06:25:10 +0200

    Remove unused syscalls and USART files from the project
    - Deleted syscalls.c, syscalls.d, syscalls.o, syscalls.su, sysmem.c, sysmem.d, sysmem.o, sysmem.su, usart.c, usart.d, usart.o, usart.su, and startup files related to STM32H755.
    - Cleaned up the project structure by removing unnecessary files that are no longer in use.

--- CM7/Debug/Drivers/BSP/Components/lan8742.d
+++
@@ -1,4 +0,0 @@
-Drivers/BSP/Components/lan8742.o: \
- C:/Users/darthrooster/Documents/STM_Dev/AntiSEL/Drivers/BSP/Components/lan8742/lan8742.c \
- C:/Users/darthrooster/Documents/STM_Dev/AntiSEL/Drivers/BSP/Components/lan8742/lan8742.h
-C:/Users/darthrooster/Documents/STM_Dev/AntiSEL/Drivers/BSP/Components/lan8742/lan8742.h:
--- CM7/Debug/Startup/startup_stm32h755zitx.d
+++
@@ -1,1 +0,0 @@
-Startup/startup_stm32h755zitx.o: ../Startup/startup_stm32h755zitx.s
